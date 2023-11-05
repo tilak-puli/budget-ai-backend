473636ad132737cecfe185b1a6b12999a5644da8
Tilak Puli <tilakpuli15@gmail.com>
2023-11-05 21:10:08 +0530

    add whatsapp expense message

--- src/routes/expenseRouter.js
+++ src/routes/expenseRouter.js
@@ -5,5 +5,6 @@ const controller = require("../controllers/expenseController")
 router.get("/expenses", controller.getExpenses)
 router.post("/ai/expense", controller.addAiExpenseWithMessage)
 router.post("/whatsapp/expense", controller.addAiExpenseFromWhatsapp)
+router.get("/whatsapp/expense", controller.addAiExpenseFromWhatsapp)
 
 module.exports = router
